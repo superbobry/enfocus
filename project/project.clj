10b3748dfb5d91db3623399c3894ce2d0437340a
Sergei Lebedev <superbobry@gmail.com>
2012-06-03 03:44:44 +0400

    Updated 'project.clj' and added 'domina' to ':dependencies'

--- project/project.clj
+++ project/project.clj
@@ -2,16 +2,11 @@
   :description "DOM manipulation tool for clojurescript inspired by Enlive"
   :source-path "cljs-src"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [goog-jar "1.0.0"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]
-                     [lein-cljsbuild "0.0.7"]]
-  :cljsbuild {
-    ; The path to the top-level ClojureScript source directory:
-    :source-path "cljs-src"
-    ; The standard ClojureScript compiler options:
-    ; (See the ClojureScript compiler documentation for details.)
-    :compiler {
-      :output-dir "../sample/resources/public/cljs" 
-      :output-to "../sample/resources/public/cljs/enfocus.js"         
-      :optimizations :whitespace
-      :pretty-print true}})
+                 [domina "1.0.0-beta4"]]
+  :plugins [[lein-eclipse "1.0.0"]
+            [lein-cljsbuild "0.2.1"]]
+  :cljsbuild {:builds [{:source-path "cljs-src"
+                        :compiler {:output-dir "../sample/resources/public/cljs"
+                                   :output-to "../sample/resources/public/cljs/enfocus.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
